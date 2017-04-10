2661b3b26136333aceb26aa42483a22795f15116
theciarabrophy <theciarabrophy@gmail.com>
2017-04-10 14:11:53 +0100

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,1 +1,15 @@
 
+//Near Attack
+	if(targetSlot.taken == true || player[i].Slot < 1)
+	{
+		if (player[i].Strength <= 70)
+		{
+			player[i].Health - (0.5 * player[i].Strength);
+		}
+		else if (player[i].Strenght > 70)
+		{
+			player[i].Health - (0.3 * player[i].Strength);
+		}
+		else 
+			return 0;
+	}
